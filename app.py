dc41501a309f8d85fc758cddda36b9d1df89def6
Japnitsingh123 <japnitsingh2005@gmail.com>
2025-06-27 14:42:05 +0530

    updated

--- app.py
+++ app.py
@@ -1,7 +1,5 @@
 ##invoice extractor
 
-from dotenv import load_dotenv
-load_dotenv() ##loads all env variables from .env
 
 import streamlit as st
 import os
@@ -9,15 +7,14 @@ from PIL import Image
 import google.generativeai  as genai
 
 ##configuring API key
-api_key=os.getenv("GOOGLE_API_KEY")
-genai.configure(api_key=api_key)
+genai.configure(api_key=st.secrets["GOOGLE_API_KEY"])
 
 ##function to load gemini model and get response
 def get_gemini_response(input, image, prompt):
-    model = genai.GenerativeModel("gemini-1.5-flash")  # ✅ updated model name
+    model = genai.GenerativeModel("gemini-1.5-flash")
 
     response = model.generate_content(
-        [input, image,prompt],  # ✅ image is now a Part, not list
+        [input, image,prompt], 
     )
     return response.text
 
@@ -25,7 +22,7 @@ def input_image_setup(uploaded_file):
     if uploaded_file is not None:
         bytes_data = uploaded_file.getvalue()
 
-        # ✅ Return a valid Part (image input) for Gemini 1.5
+        #Return image_part
         image_part = {
             "mime_type": uploaded_file.type,
             "data": bytes_data
